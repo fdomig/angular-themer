0d35163075e23a11d5fff049b2b9207bfe557fd1
Franziskus Domig <fdomig@gmail.com>
2013-11-25 12:21:53 +0100

    chore(release): v0.2.0
    Functionality is now split into two separate directives:
    
    * themer-link: Adds <link> to the header
    * themer-switcher: Adds <select> options to select available themes

--- demo/demo.js
+++ demo/demo.js
@@ -1,15 +1,16 @@
 angular.module('demoApp', ['angular-themer'])
 
+	.config(['themerProvider', function (themerProvider) {
+		var styles = [
+			{ label: 'Dark Theme', href: 'dark.css'},
+			{ label: 'Light Theme', href: 'light.css'},
+			{ label: 'Dracula Theme', href: 'dracula.css'}
+		];
+
+		themerProvider.setStyles(styles);
+		themerProvider.setSelected(styles[0]);
+	}])
+
 	.controller('DemoCtrl', function($scope) {
 		
-		$scope.theme = {
-			styles: [
-				{ label: 'Light Theme', href: 'light.css'},
-				{ label: 'Dark Theme', href: 'dark.css'},
-				{ label: 'Dracula Theme', href: 'dracula.css'}
-			]
-		};
-
-		$scope.theme.selected = $scope.theme.styles[0].href;
-
 	});
--- src/themer.js
+++ src/themer.js
@@ -1,17 +1,56 @@
 angular.module('angular-themer', [])
 
-	.directive('themer', function() {
+	.provider('themer', function() {
+		"use strict";
+
+		var _styles = [], _selected = { label: '', href: '' };
+
+		this.setStyles = function (styles) {
+			_styles = styles;
+		};
+
+		this.setSelected = function (selected) {
+			_selected = selected;
+		};
+
+		this.$get = [function () {
+			return {
+				styles: _styles,
+				selected: _selected
+			};
+		}]
+
+	})
+
+	.directive('themerLink', function() {
 		"use strict";
 
 		return {
 			restrict: 'A',
-			template:  '<link rel="stylesheet" ng-href="{{ ngModel }}" />',
+			template:  '<link rel="stylesheet" ng-href="{{ selected.href }}" />',
 			replace: true,
-			scope: {
-				ngModel: '='
-			}
+			scope: true,
+			controller: ['$scope', 'themer', function ($scope, themer) {
+				$scope.selected = themer.selected;
+			}]
 		};
 
+	})
+
+	.directive('themerSwitcher', function () {
+		"use strict";
+
+		return {
+			restrict: 'A',
+			template: '<select ng-model="theme.selected.href" ng-options="style.href as style.label for style in theme.styles"></select>',
+			scope: false,
+			controller: ['$scope', 'themer', function ($scope, themer) {
+				$scope.theme = {
+					styles: themer.styles,
+					selected: themer.selected
+				};
+			}]
+		};
 	})
 
 ;
